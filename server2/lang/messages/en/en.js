8eb2db91748e18cb449b18dcc256914055411fc2
Kiet Ngo <Kietkiet1109@yahoo.com>
2025-02-15 09:54:12 -0800

    Server 1 Branch

--- server2/lang/messages/en/en.js
+++
@@ -1,9 +0,0 @@
-const messages = {
-    NULL: "Notes cannot be empty.",
-    EMPTY: "",
-    REMOVE: "Remove",
-    ADD: "Add",
-    SUBMIT: "Submit",
-    LAST_SAVED: "Last saved: ",
-    RETURN: "Return",
-}
