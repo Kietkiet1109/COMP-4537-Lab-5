478d74ca17f3109b079fa3e37604d03a14d73314
Kiet Ngo <Kietkiet1109@yahoo.com>
2025-02-15 22:18:12 -0800

    Server 2 Update

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "Kiet",
-    password: "YeuHoangKimTu5Tuoi@",
-    database: "sys"
+    socketPath: "/cloudsql/sharp-imprint-451101-e6:us-central1:comp4537labs5",
+    user: "root",
+    password: "kiet",
+    database: "sys",
+    port: 3306,
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    if (err) {
+        console.error("Connection failed with error: ", err);
+        return;
+    }
     console.log("Connected!");
 });
 
